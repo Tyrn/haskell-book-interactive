f2e34c58697e2960c0a914c61d76c629a1aaf37d
Tyrn <alexey1911@gmail.com>
2026-09-26 12:21:31 +0300

    hspec-discover works, but ruins HLS

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,15 +1,17 @@
-import Ch02Spec qualified
-import Ch03Spec qualified
-import Ch04Spec qualified
-import Ch05Spec qualified
-import Test.DocTest (doctest)
-import Test.Hspec
+{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
 
-main :: IO ()
-main = do
-  doctest ["-isrc", "src"]
-  hspec $ do
-    describe "Chapter 2: Hello, Haskell!" Ch02Spec.spec
-    describe "Chapter 3: Strings" Ch03Spec.spec
-    describe "Chapter 4: Basic datatypes" Ch04Spec.spec
-    describe "Chapter 5: Types" Ch05Spec.spec
+-- import Ch02Spec qualified
+-- import Ch03Spec qualified
+-- import Ch04Spec qualified
+-- import Ch05Spec qualified
+-- import Test.DocTest (doctest)
+-- import Test.Hspec
+--
+-- main :: IO ()
+-- main = do
+--   doctest ["-isrc", "src"]
+--   hspec $ do
+--     describe "Chapter 2: Hello, Haskell!" Ch02Spec.spec
+--     describe "Chapter 3: Strings" Ch03Spec.spec
+--     describe "Chapter 4: Basic datatypes" Ch04Spec.spec
+--     describe "Chapter 5: Types" Ch05Spec.spec
